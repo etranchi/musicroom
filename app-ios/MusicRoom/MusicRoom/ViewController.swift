b2cce43d93dd0216d9158b34a262d62278255fde
etranchi <etranchi@student.42.fr>
2018-10-22 21:27:02 +0200

    init

---
+++ app-ios/MusicRoom/MusicRoom/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MusicRoom
+//
+//  Created by Etienne Tranchier on 22/10/2018.
+//  Copyright © 2018 Etienne Tranchier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("coucou")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
