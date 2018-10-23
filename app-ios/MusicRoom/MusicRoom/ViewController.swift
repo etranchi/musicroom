6fa699d6e7167c8e202467ab046709a4fb734093
etranchi <etranchi@student.42.fr>
2018-10-23 11:59:20 +0200

    SDK working

--- app-ios/MusicRoom/MusicRoom/ViewController.swift
+++ app-ios/MusicRoom/MusicRoom/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+let APP_ID = "306764"
+class ViewController: UIViewController , DeezerSessionDelegate{
+    var connect : DeezerConnect?
+    var session : DeezerSessionDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
         print("coucou")
+        connect = DeezerConnect()
+        connect = DeezerConnect.init(appId: APP_ID, andDelegate: session.self)
+        print(session?.deezerDidLogin!())
+        print(connect?.accessToken)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ app-ios/MusicRoom/MusicRoom/bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  MusicRoom
+//
+//  Created by Etienne Tranchier on 23/10/2018.
+//  Copyright © 2018 Etienne Tranchier. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "../DeezerSDK/include/Deezer+Compatibility.h"
+#import "../DeezerSDK/include/DZRPodcast.h"
+
+#endif /* bridge_h */
