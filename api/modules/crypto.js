f779f5e34504d51a852991e0899f68d1cb710e9a
kevin vignau <kvignau@gamil.com>
2018-10-27 12:27:38 +0200

    add yarn install to react and remove sodium lib

--- api/modules/crypto.js
+++ api/modules/crypto.js
@@ -1,19 +1,9 @@
 'use strict';
 
-const sodium = require('sodium').api;
 const jwt = require('jsonwebtoken');
 const config = require('../config/config.json');
 
 class Crypto {
-	static randomString(size = 16, charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') {
-		let rand = '';
-		sodium.randombytes_stir();
-		for (let i = 0; i < size; i++) {
-			rand += charset[sodium.randombytes_uniform(charset.length - 1)];
-		}
-		return rand;
-	}
-
 	static createToken(user) {
 		let token = jwt.sign({
 			id: user._id,
