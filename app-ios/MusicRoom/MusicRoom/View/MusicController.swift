80d69fa9c2b4421c29c1af607357b0a548dc7de1
etranchi <etranchi@student.42.fr>
2018-11-02 18:56:45 +0100

    add footer bar for player

--- app-ios/MusicRoom/MusicRoom/View/MusicController.swift
+++ app-ios/MusicRoom/MusicRoom/View/MusicController.swift
@@ -12,6 +12,7 @@ class MusicController: UIViewController, UITableViewDelegate, UITableViewDataSou
     @IBOutlet weak var tableView: UITableView!
     var tracks : [Track]?
     var selectedTrack : Int = 0
+    var footerView : UIView?
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tracks!.count
     }
@@ -30,7 +31,19 @@ class MusicController: UIViewController, UITableViewDelegate, UITableViewDataSou
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        footerView = UIView(frame: CGRect(x: 0, y: (self.view.frame.height - 50), width: self.view.frame.width, height: 50))
+        footerView?.backgroundColor = .red
+        let backButton = UIButton(frame: CGRect(x: (footerView?.frame.width)! / 4 - 10 , y: 10, width: 20, height: 10))
+        backButton.backgroundColor = .white
+        footerView?.addSubview(backButton)
+        let playButton = UIButton(frame: CGRect(x: (footerView?.frame.width)! / 2 - 10, y: 10, width: 20, height: 10))
+        playButton.backgroundColor = .white
+        footerView?.addSubview(playButton)
+        let nextButton = UIButton(frame : CGRect(x: (((footerView?.frame.width)! * 3) / 4) - 10, y: 10, width: 20, height: 10))
+        nextButton.backgroundColor = .white
+        footerView?.addSubview(nextButton)
+        self.view.addSubview(footerView!)
+        
         // Do any additional setup after loading the view.
     }
 
