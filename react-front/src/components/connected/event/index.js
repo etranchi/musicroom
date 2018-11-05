06fe405b0a63a1594ec99ed6eebc6aa7212764bc
Edouard EBERSOLDT <eebersol@e2r4p13.42.fr>
2018-11-05 18:17:24 +0100

    ...

--- react-front/src/components/connected/event/index.js
+++ react-front/src/components/connected/event/index.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import {Tabs, Tab} from 'react-materialize'
 import Create from './createEvent';
 import List from './listEvent';
+import CardEvent from './completCardEvent';
 import './styles.css';
 
 class Event extends Component {
+	constructor(props){
+		super(props);
+	}
 	render() {
-	return (
-		<Tabs className='tab-demo z-depth-1'>
-			<Tab title="Créer un Event"> <Create /> </Tab>
-			<Tab title="Liste de vos Events" active><List /></Tab>
-			<Tab title="Liste des évents à proximité" ></Tab>
-			<Tab title="Mes invitations"> </Tab>
-		</Tabs>
-	);
-  }
+		{console.log("Base event : ", this.props.state.currentComponent)}
+		if (this.props.state.currentComponent === "cardEvent") {
+			console.log("Get card")
+			return (<CardEvent state={this.props.state} updateParent={this.props.updateParent} />)
+		}
+		else {
+			return (
+				<Tabs className='tab-demo z-depth-1'>
+					<Tab title="Créer un Event"> <Create state={this.props.state} updateParent={this.props.updateParent}/> </Tab>
+					<Tab title="Liste de vos Events" active><List state={this.props.state} updateParent={this.props.updateParent}/></Tab>
+					<Tab title="Liste des évents à proximité" ></Tab>
+					<Tab title="Mes invitations"> </Tab>
+				</Tabs>
+			);
+		}
+	}
 }
 
 export default Event;
--- react-front/src/components/connected/event/listEvent/index.js
+++ react-front/src/components/connected/event/listEvent/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Card, Avatar, Icon } from 'antd';
 import './styles.css';
+import PreviewCard from '../previewCardEvent';
 import axios from 'axios'
 
 class ListEvent extends Component {
@@ -9,43 +9,8 @@ class ListEvent extends Component {
         this.state = {
 			events: []
 		}
-		this.card = {
-			minHeight:"300px",
-			margin: "5% 0 0 0"
-		}
-		this.gridStyle = {
-			minHeight:"300px",
-			width: '25%',
-			textAlign: 'center',
-		};
-		this.gridStylePicture = {
-			width: '50%',
-			padding: "0 0 0 0"
-		};
-		this.eventPicture = {
-			height:"300px",
-			width: '100%'
-		};
-		this.iconStyle = {
-			fontSize: '18px',
-			float: "left"
-		
-		}
-		this.iconEditionStyle = {
-			padding: "0 7% 0 7% ",
-			fontSize: '18px',
-		}
-		this.iconEditionBlockStyle = {
-			margin: "50% 0 0 0"
-		}
-		this.iconBlockStyle = {
-			margin: "8% 0 0 0"
-		}
-		this.descritpionBlockStyle = {
-			margin: "50% 0 0 0"
-		}
+		this.onLoad = false;
 	}
-		
 	componentDidMount = () => {
 		console.log('REQUEST')
 		axios.get('https://192.168.99.100:4242/event')
@@ -64,57 +29,23 @@ class ListEvent extends Component {
 
     
 	render() {
-		console.log(this.state.events, this.state.events.length)
+		if (this.props.state.currentComponent != "listEvent" && this.onLoad === false) {
+			this.onLoad = true;
+			{this.componentDidMount()}
+		}
 		if(this.state.events.length === 0 ) {
-			console.log("IVI")
+			this.onLoad = false;
 			return <div>Loading...</div>
 		}
 		else
 		{
-			console.log("EVENT : ")
+			{this.props.updateParent.bind(this,{'currentComponent': 'listEvent', 'data': this.state.events})}
 			return (
 				<div>
-				{ console.log(this.state.events)}
 				{
 					this.state.events.map((event) => {
 							return (
-									<Card title={event.title} style={this.card}>
-										<Card.Grid style={this.gridStyle}>
-											<Card.Meta
-											avatar={<Avatar src={"https://192.168.99.100:4242/eventPicture/" +  event.picture} />}
-											title= {event.creator && event.creator.login ? event.creator.login : "Aucun" }
-											/>
-											<div style={this.descritpionBlockStyle}>
-												<b> { event.descritpion ? event.descritpion : "Aucune descritpion ..." }</b>
-											</div>
-											<div style={this.iconEditionBlockStyle}>
-											<Icon style={this.iconEditionStyle} type="setting" theme="outlined" />
-											<Icon style={this.iconEditionStyle} type="edit" theme="outlined" />
-											<Icon style={this.iconEditionStyle} type="delete" theme="outlined" />
-											</div>
-										</Card.Grid>
-										<Card.Grid style={this.gridStyle}>
-										<div style={this.iconBlockStyle}>
-											<Icon  style={this.iconStyle} type="pushpin" theme="outlined" />
-											<b style={this.iconNameStyle}>Paris {/* {this.event.address.v} */} </b>
-										</div>
-										<div style={this.iconBlockStyle}>
-											<Icon  style={this.iconStyle} type="clock-circle" theme="outlined" />
-											<b style={this.iconNameStyle}> { event.date_creation ? event.date_creation : " à définir .." }</b>
-										</div>
-										<div style={this.iconBlockStyle}>
-											<Icon  style={this.iconStyle} type={ event.public ? "unlock" : "lock" } theme="outlined" />
-											<b style={this.iconNameStyle}> { event.public ? " Public" : " Privé" }</b>
-										</div>
-										<div style={this.iconBlockStyle}>
-											<Icon style={this.iconStyle} type="user" theme="outlined" />
-											<b style={this.iconNameStyle}> { event.members.count ? event.members.count + " participants" : "0 participant" }</b>
-										</div>
-										</Card.Grid>
-										<Card.Grid style={this.gridStylePicture}>
-											<img style={ this.eventPicture} alt="example" src={"https://192.168.99.100:4242/eventPicture/" +  event.picture} />
-										</Card.Grid>
-									</Card>
+								<PreviewCard event={event} updateParent={this.props.updateParent}/>
 							)
 						})
 				}
