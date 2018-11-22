6b93197ca64971da5f9f2d143c0a9a424e01f0df
Edouard EBERSOLDT <eebersol@e2r8p18.42.fr>
2018-11-22 12:30:25 +0100

    dd

--- react-front/src/components/connected/sockets/index.js
+++ react-front/src/components/connected/sockets/index.js
@@ -1,11 +1,7 @@
 import openSocket from 'socket.io-client';
 
-<<<<<<< HEAD
 const socket = openSocket('https://192.168.99.100:4242');
 
-=======
-const socket = openSocket(process.env.REACT_APP_API_URL);
->>>>>>> 8579b7d214e387e087e5519af50d83e8e62eb08a
 function moveMusic(playlistId) {
     socket.emit('moveMusic', playlistId);
 }
