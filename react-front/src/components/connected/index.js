532764a58bfed47cabfcb398f2ac6108db14d40f
Edouard EBERSOLDT <eebersol@e2r5p15.42.fr>
2018-11-27 15:51:03 +0100

    Add some featur eon event

--- react-front/src/components/connected/index.js
+++ react-front/src/components/connected/index.js
@@ -20,8 +20,6 @@ class Connected extends Component {
 		}
 
 		componentWillMount(){
-			console.log("ici");
-			console.log("toto");
 			this.getGeolocalisation();
 			this.getEvents();
 		}
@@ -31,16 +29,20 @@ class Connected extends Component {
 				console.log("first if");
 				this.props.state.data.userCoord = {}
 				navigator.geolocation.getCurrentPosition( (position ) => {
-					console.log("getcurrentposition");
 					this.props.state.data.userCoord.lat = position.coords.latitude
 					this.props.state.data.userCoord.lng = position.coords.longitude
+					console.log("ICI 1")
 					this.props.updateParent({'data': this.props.state.data})
+					console.log("ICI 2")
+					console.log(this.props.state.data.userCoord)
+
+
+
 				}, (err) => {
 					console.log("error");
 					console.log(err);
 					axios.get('https://geoip-db.com/json/')
 					.then(location => {
-						console.log("then");
 						this.props.state.data.userCoord.lat = location.data.latitude
 						this.props.state.data.userCoord.lng = location.data.longitude
 						this.props.updateParent({'data': this.props.state.data})
@@ -48,7 +50,6 @@ class Connected extends Component {
 					})
 					.catch(err => {
 						console.log('error 2 ' + err);
-
 						this.props.state.data.userCoord.lat = 0
 						this.props.state.data.userCoord.lng = 0
 						this.props.updateParent({'data': this.props.state.data})
@@ -75,6 +76,11 @@ class Connected extends Component {
 				this.setState({
 					collapsed: !this.state.collapsed,
 				});
+		}
+
+		openCard = (event) => {
+			this.props.state.data.event = event;
+			this.props.updateParent({'currentComponent': 'cardEvent', 'data': this.props.state.data})
 		}
 
 
