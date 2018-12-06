78a942a71ad52c25bdaf56e77670cb5ccf8b2c55
jdavin <jdavin@student.42.fr>
2018-12-06 08:55:18 +0100

    good

--- api/routes/album.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const express = require('express');
-const router = express.Router();
-const albumController = require('../controllers/album');
-
-router.get('/:id', albumController.getTracksByAlbum);
-
-module.exports = router;
