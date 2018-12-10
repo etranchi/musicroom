abf33804da405631d335e52f05d971b926df2e13
Kevin VIGNAU <kvignau@e2r5p14.42.fr>
2018-12-10 12:03:31 +0100

    search with promise and get albums

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -1,49 +1,54 @@
 const config = require('../config/config');
-const request = require('request');
 const customError = require('../modules/customError');
 const playlistModel 	= require('../models/playlist');
 const moduleUrl = '/search';
-const requestPromise = require('request-promise');
+const request = require('request-promise');
 
 module.exports = {
-	search: (req, res, next) => {
-		if (Object.keys(req.query).length === 0)
-			res.status(400).json("error");
-		console.log(req.query);
-		console.log(config.deezer.apiUrl + moduleUrl + '?q=' + req.query.q);
-		request(
-			{uri: config.deezer.apiUrl + moduleUrl + '?q=' + req.query.q},
-			(err, head, body) => {
-				if (err)
-					next(new customError(err.message, err.code))
-				res.json(JSON.parse(body));
-			})
+	search: async (req, res, next) => {
+		try {
+			if (Object.keys(req.query).length === 0 || req.query.q === "")
+				res.status(200).send([])
+			let options = {
+				method: 'GET',
+				uri: config.deezer.apiUrl + moduleUrl + '?q=' + encodeURIComponent(req.query.q),
+				json: true
+			};
+			let search = await request(options)
+			res.status(200).send(search.data)
+		} catch (err) {
+			next(new customError(err.message, 400))
+		}
 	},
-	searchAlbum: (req, res, next) => {
-		if (Object.keys(req.query).length === 0)
-			res.status(400).json("error");
-		console.log(req.query);
-		console.log(config.deezer.apiUrl + moduleUrl + '?q=' + req.query.q);
-		request(
-			{uri: config.deezer.apiUrl + moduleUrl + '/album ' + '?q=' + req.query.q},
-			(err, head, body) => {
-				if (err)
-					next(new customError(err.message, err.code))
-				res.json( JSON.parse(body));
-			})
+	searchAlbum: async (req, res, next) => {
+		try {
+			if (Object.keys(req.query).length === 0 || req.query.q === "")
+				res.status(200).send([])
+			let options = {
+				method: 'GET',
+				uri: config.deezer.apiUrl + moduleUrl + '/album ' + '?q=' + encodeURIComponent(req.query.q),
+				json: true
+			};
+			let album = await request(options)
+			res.status(200).send(album.data)
+		} catch (err) {
+			next(new customError(err.message, 400))
+		}
 	},
-	searchTrack: (req, res, next) => {
-		if (Object.keys(req.query).length === 0)
-			res.status(400).json("error");
-		console.log(req.query);
-		console.log(config.deezer.apiUrl + moduleUrl + '?q=' + req.query.q);
-		request(
-			{uri: config.deezer.apiUrl + moduleUrl + '/track ' + '?q=' + req.query.q},
-			(err, head, body) => {
-				if (err)
-					next(new customError(err.message, err.code))
-				res.json(JSON.parse(body));
-			})
+	searchTrack: async (req, res, next) => {
+		try {
+			if (Object.keys(req.query).length === 0 || req.query.q === "")
+				res.status(200).send([])
+			let options = {
+				method: 'GET',
+				uri: config.deezer.apiUrl + moduleUrl + '/track ' + '?q=' + encodeURIComponent(req.query.q),
+				json: true
+			};
+			let tracks = await request(options)
+			res.status(200).send(tracks.data)
+		} catch (err) {
+			next(new customError(err.message, 400))
+		}
 	},
 	searchPlaylist: async  (req, res, next) => {
 		try {
@@ -62,7 +67,7 @@ module.exports = {
 				uri: config.deezer.apiUrl  + moduleUrl + '/playlist?q=' + encodeURIComponent(req.query.q),
 				json: true
 			};
-			playlist = await requestPromise(options)
+			playlist = await request(options)
 			if (playlist.total > 0) {
 				allPlaylist = allPlaylist.concat(playlist.data)
 			}
@@ -75,17 +80,19 @@ module.exports = {
 			next(new customError(err.message, 400))
 		}
 	},
-	searchArtist: (req, res, next) => {
-		if (Object.keys(req.query).length === 0)
-			res.status(400).json("error");
-		console.log(req.query);
-		console.log(config.deezer.apiUrl + moduleUrl + '?q=' + req.query.q);
-		request(
-			{uri: config.deezer.apiUrl + moduleUrl + '/artist ' + '?q=' + req.query.q},
-			(err, head, body) => {
-				if (err)
-					next(new customError(err.message, err.code))
-				res.json(JSON.parse(body));
-			})
+	searchArtist: async (req, res, next) => {
+		try {
+			if (Object.keys(req.query).length === 0 || req.query.q === "")
+				res.status(200).send([])
+			let options = {
+				method: 'GET',
+				uri: config.deezer.apiUrl + moduleUrl + '/artist ' + '?q=' + encodeURIComponent(req.query.q),
+				json: true
+			};
+			let artist = await request(options)
+			res.status(200).send(artist.data)
+		} catch (err) {
+			next(new customError(err.message, 400))
+		}
 	}
 }
